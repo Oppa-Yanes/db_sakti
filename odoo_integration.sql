cf8d18f456dcf0fc37bf29e1421b68fb4c0e3c86
Oppa Yanes <yanes.bahari@gmail.com>
2026-03-10 16:15:06 +0700

    Update odoo_integration.sql
    -- Hasil Panen - Lines

--- odoo_integration.sql
+++ odoo_integration.sql
@@ -1,4 +1,4 @@
--- Hasil Panen Header
+-- Hasil Panen - Header
 WITH params AS (
 	SELECT
 		DATE '2026-03-09' current_date
@@ -28,22 +28,22 @@ block_ids AS (
 		hv.harvest_date::DATE
 )
 SELECT
-	f.id sakti_id,
+	bkm.id bkm_id,
 	rkh.rkh_date date,
 	h.is_holiday,
 	bids.planted_block_ids,
 	--f.is_kutip_required,
-	f.foreman_group_id,
-	f.foreman_id,
-	f.foreman1_id,
-	f.kerani_harvest_id,
+	bkm.foreman_group_id,
+	bkm.foreman_id,
+	bkm.foreman1_id,
+	bkm.kerani_harvest_id,
 	est.operating_unit_id,
 	rkh.estate_id,
 	com.id company_id
 FROM
-	sakti_foreman f
-	LEFT JOIN sakti_rkh rkh ON rkh.id = f.rkh_id
-	LEFT JOIN block_ids bids ON bids.foreman_id = f.id AND bids.harvest_date = rkh.rkh_date
+	sakti_foreman bkm
+	LEFT JOIN sakti_rkh rkh ON rkh.id = bkm.rkh_id
+	LEFT JOIN block_ids bids ON bids.foreman_id = bkm.id AND bids.harvest_date = rkh.rkh_date
 	LEFT JOIN plantation_estate est ON est.id = rkh.estate_id
 	LEFT JOIN res_company com ON com.id = rkh.company_id
 	JOIN holiday h ON TRUE
@@ -51,3 +51,28 @@ FROM
 WHERE
 	rkh.rkh_date = p.current_date 
 ;
+
+-- Hasil Panen - Lines
+WITH params AS (
+	SELECT
+		'41d68b8e-4d3c-4f4b-8232-9b9997febd8c'::UUID bkm_id
+)
+SELECT 
+	hvr.emp_id,
+	hvr.attendance_type_id,
+	loc.block_id,
+	hv.tph_id,
+	bkm.ha_amt,
+	hv.bunch_qty,
+	hv.loose_fruit_qty 
+FROM
+	sakti_harvest hv
+	LEFT JOIN sakti_harvester hvr ON hvr.id = hv.harvester_id 
+	LEFT JOIN sakti_location loc ON loc.id = hv.location_id 
+	LEFT JOIN sakti_bkm bkm ON bkm.harvester_id = hv.harvester_id AND bkm.location_id = hv.location_id
+	JOIN params p ON TRUE
+WHERE
+	hvr.foreman_id = p.bkm_id
+;
+
+
