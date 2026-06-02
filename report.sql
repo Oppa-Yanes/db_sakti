392a29bde71f28a330d2ab58bc7cfdb60851643f
Oppa Yanes <yanes.bahari@gmail.com>
2026-06-02 12:47:35 +0700

    Update Odoo vs SAKTI

--- report.sql
+++ report.sql
@@ -119,131 +119,6 @@ ORDER BY
 	loc.block_code
 ;
 
--- Laporan Akurasi ODOO vs SAKTI per Pemanen
--- URL:
-WITH params AS (
-    SELECT
-    	'20260511' AS start_date,
-    	'20260511' AS end_date,    	
-    	1 AS company_id,
-    	14 AS estate_id,
-    	--NULL::INT AS estate_id,
-    	NULL::INT AS division_id
-    	--63 AS division_id
-),
-odoo AS (
-	SELECT
-		hv.date::DATE harvest_date,
-		batch.company_id,
-		batch.estate_id,
-		dept.division_id,
-		batch.foreman_group_id,
-		batch.kerani_id kerani_harvest_id,
-		hv.employee_id emp_id,
-		COALESCE(SUM(hv.harvest_area), 0) ha_qty,
-		COALESCE(SUM(hv.bunches_qty), 0) jjg_qty,
-		COALESCE(SUM(hv.loose_qty), 0) brd_qty
-	FROM
-		plantation_harvest hv
-		LEFT JOIN plantation_batch_harvest batch ON batch.id = hv.harvest_batch_id 
-		LEFT JOIN hr_foreman_group fg ON fg.id = batch.foreman_group_id 
-		LEFT JOIN hr_department dept ON dept.id = fg.department_id 
-		JOIN params p ON TRUE
-	WHERE
-		--batch.state = 'done'
-		batch.company_id = p.company_id 
-		AND (p.estate_id IS NULL OR batch.estate_id = p.estate_id)
-		AND (p.division_id IS NULL OR dept.division_id = p.division_id)
-		AND TO_CHAR(batch.date, 'YYYYMMDD') BETWEEN p.start_date AND p.end_date 
-	GROUP BY
-		hv.date,
-		batch.company_id,
-		batch.estate_id,
-		dept.division_id,
-		batch.foreman_group_id,
-		batch.kerani_id,
-		hv.employee_id	
-),
-sakti AS (
-	SELECT
-		hv.harvest_date::DATE harvest_date,
-		rkh.company_id,
-		rkh.estate_id,
-		rkh.division_id,
-		batch.foreman_group_id,
-		batch.kerani_harvest_id,
-		hvt.emp_id,
-		COALESCE(bkm.ha_qty, 0) ha_qty,
-		COALESCE(SUM(hv.bunch_qty), 0) jjg_qty,
-		COALESCE(SUM(hv.loose_fruit_qty), 0) brd_qty
-	FROM
-		sakti_harvest hv
-		LEFT JOIN sakti_harvester hvt ON hvt.sakti_id = hv.harvester_id
-		LEFT JOIN sakti_foreman batch ON batch.sakti_id = hvt.foreman_id
-		LEFT JOIN sakti_rkh rkh ON rkh.sakti_id = batch.rkh_id
-		LEFT JOIN (
-			SELECT
-				bkm.harvester_id,
-				COALESCE(SUM(bkm.ha_amt), 0) ha_qty
-			FROM
-				sakti_bkm bkm
-			GROUP BY
-				bkm.harvester_id 
-			) bkm ON bkm.harvester_id = hvt.sakti_id
-		JOIN params p ON TRUE
-	WHERE
-		rkh.company_id = p.company_id 
-		AND (p.estate_id IS NULL OR rkh.estate_id = p.estate_id)
-		AND (p.division_id IS NULL OR rkh.division_id = p.division_id)
-		AND TO_CHAR(hv.harvest_date, 'YYYYMMDD') BETWEEN p.start_date AND p.end_date 
-	GROUP BY
-		hv.harvest_date::DATE,
-		rkh.company_id,
-		rkh.estate_id,
-		rkh.division_id,
-		batch.foreman_group_id,
-		batch.kerani_harvest_id,
-		hvt.emp_id,
-		bkm.ha_qty
-)
-SELECT
-    COALESCE(odoo.harvest_date, sakti.harvest_date) harvest_date,
-    COALESCE(odoo.company_id, sakti.company_id) company_id,
-    coy.name company_name,
-    COALESCE(odoo.estate_id, sakti.estate_id) estate_id,
-    est.name estate_name,
-    COALESCE(odoo.division_id, sakti.division_id) division_id,
-    div.name division_name,
-    COALESCE(odoo.foreman_group_id, sakti.foreman_group_id) foreman_group_id,
-    fg.name foreman_group,
-    mandor.id foreman_id,
-    mandor.name foreman_name,
-    kerani.id kerani_id,
-    kerani.name kerani_name,
-    COALESCE(odoo.emp_id, sakti.emp_id) harvester_id,
-    harvester.name harvester_name,
-    harvester.nomor_induk_pegawai harvester_nip,
-    COALESCE(odoo.ha_qty, 0) odoo_ha_qty,
-    COALESCE(sakti.ha_qty, 0) sakti_ha_qty,
-    COALESCE(sakti.ha_qty / NULLIF(odoo.ha_qty, 0), 0) ha_acc,
-    COALESCE(odoo.jjg_qty, 0) odoo_jjg_qty,
-    COALESCE(sakti.jjg_qty, 0) sakti_jjg_qty,
-    COALESCE(sakti.jjg_qty / NULLIF(odoo.jjg_qty, 0), 0) jjg_acc,
-    COALESCE(odoo.brd_qty, 0) odoo_brd_qty,
-    COALESCE(sakti.brd_qty, 0) sakti_brd_qty,
-    COALESCE(sakti.brd_qty / NULLIF(odoo.brd_qty, 0), 0) brd_acc
-FROM
-	odoo
-	FULL JOIN sakti ON sakti.emp_id = odoo.emp_id
-	LEFT JOIN res_company coy ON coy.id = COALESCE(odoo.company_id, sakti.company_id)
-	LEFT JOIN plantation_estate est ON est.id = COALESCE(odoo.estate_id, sakti.estate_id)
-	LEFT JOIN plantation_division div ON div.id = COALESCE(odoo.division_id, sakti.division_id)
-	LEFT JOIN hr_foreman_group fg ON fg.id = COALESCE(odoo.foreman_group_id, sakti.foreman_group_id)
-	LEFT JOIN hr_employee mandor ON mandor.id = fg.foreman_id 
-	LEFT JOIN hr_employee kerani ON kerani.id = fg.kerani_harvest_id 
-	LEFT JOIN hr_employee harvester ON harvester.id = COALESCE(odoo.emp_id, sakti.emp_id)
-;
-
 -- Laporan Akurasi ODOO vs SAKTI per Kerani Panen
 -- URL:
 WITH params AS (
@@ -370,7 +245,6 @@ result_set AS (
 		LEFT JOIN hr_employee harvester ON harvester.id = COALESCE(odoo.harvester_id, sakti.harvester_id)
 )
 SELECT
-	rs.harvest_date,
 	rs.company_id,
 	rs.company_name,
 	rs.estate_id,
@@ -396,7 +270,6 @@ SELECT
 FROM
 	result_set rs
 GROUP BY
-	rs.harvest_date,
 	rs.company_id,
 	rs.company_name,
 	rs.estate_id,
@@ -411,7 +284,6 @@ GROUP BY
 	rs.harvester_id,
 	rs.harvester_name
  ORDER BY
- 	rs.harvest_date,
 	rs.company_name,
 	rs.estate_name,
 	rs.division_name,
